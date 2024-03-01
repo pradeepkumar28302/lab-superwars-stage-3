18aedc1b5fe2f2b96664ac0cbd5a0233eff79463
pradeepkumar28302 <nkpradeepkumar28@gmail.com>
2024-03-01 16:11:53 +0530

    added js file for advanced for the superheros

--- src/app.js
+++ src/app.js
@@ -23,11 +23,25 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
+    //let detailedPlayers = '';
 
     // Instead of forloop use Map method
     // Code here
-
+    let detailedPlayers=players.map(function(players,i){
+        if(i%2===0){
+            var type="hero"
+        }
+        else{
+            var type="villain"
+        }
+        const details={
+            name:players,
+            strength:getRandomStrength(),
+            image:`images/super-${i+1}.png`,
+            type:type,
+        };
+        return details;
+    });
     return detailedPlayers;
 }
 
@@ -38,11 +52,42 @@ const getRandomStrength = () => {
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = "";
 
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    const hero=players.filter(function(obj){
+        return obj.type==="hero";
+    });
+
+    const villain=players.filter(function(obj){
+        return obj.type==="villain";
+    });
+
+    if(type==="hero"){
+        //creating a innerHTML array for each player
+        var x=hero.map(function(players){
+            let frag=`<div class="player">
+            <img src="${players.image}" alt="">
+            <div class="name">${players.name}</div>
+            <div class="strength">${players.strength}</div>
+            </div>`;
+            return frag
+        });
+        fragment=x.join(" ")
+    }
+    else{
+        var x=villain.map(function(players){
+            let frag=`<div class="player">
+            <img src="${players.image}" alt="">
+            <div class="name">${players.name}</div>
+            <div class="strength">${players.strength}</div>
+            </div>`;
+            return frag
+        });
+        fragment=x.join(" ")
+    }
 
     return fragment;
 }
